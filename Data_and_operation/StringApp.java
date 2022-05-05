52f6930f26b4120158b700bd97dc692fc524f4e6
ella0106 <ella0106@naver.com>
2022-05-05 21:45:03 +0900

    Create StringApp.java

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,18 @@
+public class StringApp {
+	public static void main(String[] args) {
+		System.out.println("Hello World"); // String
+		System.out.println('H'); // Character
+		System.out.println("H");
+		
+		System.out.println("Hello"
+				+ "World"); // 다른 언어들처럼 줄바꿈만 하면 인식하지 않음
+		
+		// new line
+		System.out.println("Hello \nWorld");
+		
+		// escape
+		System.out.println("Hello \"World\""); // Hello "World"
+
+	}
+
+}
